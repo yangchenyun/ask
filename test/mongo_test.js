b0616b6535c4d2def36c8e842842d1e8c4e2538c
kuno <neokuno@gmail.com>
2012-10-27 16:40:56 +0800

    merged

--- test/mongo_test.js
+++ test/mongo_test.js
@@ -2,25 +2,25 @@ var util = require('util');
 var askMongo = require('../lib/mongo.js').init('test');
 var data = require('./data.json');
 
-// Adding tests
+// // Adding tests
 data.forEach(function(command) {
     askMongo.add(command, function(err, result) {
         //return console.log(result);
     });
 });
 
-// Query tests
-askMongo.get({id:13}, function(err, result) {
-//    console.log(result);
+// // Query tests
+askMongo.getCommand(3, function(err, result) {
+    //console.log(result);
 });
 
-askMongo.get({programs:'tee'}, function(err, result) {
-//    console.log(result);
+askMongo.query('sudo', function(err, result) {
+    //console.log(result);
 });
-
-askMongo.get({programs:'sudo'}, function(err, result) {
-    console.log(result);
-    askMongo.collection.drop();
-    askMongo.db.close();
-    process.exit(0);
+ 
+askMongo.query('sudo python', function(err, result) {
+     console.log(result);
+     askMongo.collection.drop();
+     askMongo.db.close();
+     process.exit(0);
 });
