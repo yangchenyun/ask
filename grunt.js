8da40717aabcd75365b356bfbffabeb4eb860b1f
kuno <neokuno@gmail.com>
2012-11-01 22:21:26 +0800

    not use jsbeautifier here

--- grunt.js
+++ grunt.js
@@ -34,33 +34,33 @@ module.exports = function(grunt) {
     jsvalidate: {
       files: ['./grunt.js', './bin/*', './lib/*.js', 'test/*.js'],
     },
-    jsbeautifier: {
-      files: ['./grunt.js', './bin/*', './lib/*.js', 'test/*.js'],
-      options: {
-        "indent_size": 2,
-        "indent_char": " ",
-        "indent_level": 0,
-        "indent_with_tabs": false,
-        "preserve_newlines": true,
-        "max_preserve_newlines": 10,
-        "jslint_happy": false,
-        "brace_style": "collapse",
-        "keep_array_indentation": false,
-        "keep_function_indentation": false,
-        "space_before_conditional": true,
-        "eval_code": false,
-        "indent_case": false,
-        "unescape_strings": false
-      }
-    }
+    // jsbeautifier: {
+    //   files: ['./grunt.js', './bin/*', './lib/*.js', 'test/*.js'],
+    //   options: {
+    //     "indent_size": 2,
+    //     "indent_char": " ",
+    //     "indent_level": 0,
+    //     "indent_with_tabs": false,
+    //     "preserve_newlines": true,
+    //     "max_preserve_newlines": 10,
+    //     "jslint_happy": false,
+    //     "brace_style": "collapse",
+    //     "keep_array_indentation": false,
+    //     "keep_function_indentation": false,
+    //     "space_before_conditional": true,
+    //     "eval_code": false,
+    //     "indent_case": false,
+    //     "unescape_strings": false
+    //   }
+    // }
 
   });
 
   //
   grunt.loadNpmTasks('grunt-jsvalidate');
-  grunt.loadNpmTasks('grunt-jsbeautifier');
+  //grunt.loadNpmTasks('grunt-jsbeautifier');
 
-  // Default task.
-  grunt.registerTask('default', 'jsvalidate jsbeautifier');
+  // Default task.                        u
+  grunt.registerTask('default', 'jsvalidate');
 
 };
